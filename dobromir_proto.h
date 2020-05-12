d6f38cab604f1911be9d26427764ff223f8f5cbf
Wojciech Franczyk <wojciech.franczyk@posteo.net>
2020-05-12 23:29:17 +0100

    proto first struct

--- dobromir_proto.h
+++ dobromir_proto.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct dbr_usb_request_hdr
+{
+    uint8_t request_type;
+    uint8_t request_nb;
+    uint16_t value;
+    uint16_t index;
+    uint16_t length;
+};
+
+
+#ifdef __cplusplus
+}
+#endif
