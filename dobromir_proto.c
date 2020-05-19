73228cbbeacd0c1e6e67690d8c1dd0bf253bf5db
Wojciech Franczyk <wojciech.franczyk@posteo.net>
2020-05-19 22:00:52 +0100

    ping report

--- dobromir_proto.c
+++ dobromir_proto.c
@@ -2,5 +2,15 @@
 
 void init_report_peripherals(struct report_peripherals* report)
 {
-  *report = {REPORT_PERIPHERALS_ID, 0, 0, 0, 0};
+  report->id = REPORT_PERIPHERALS_ID;
+  report->joy_x = 0;
+  report->joy_y = 0;
+  report->axis_z = 0;
+  report->buttons = 0;
 } 
+
+void init_report_ping(struct report_ping* report)
+{
+  report->id = REPORT_PING_ID;
+  report->dummy = 0x9;
+}
--- dobromir_proto.h
+++ dobromir_proto.h
@@ -5,7 +5,9 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-  
+
+#define REPORT_SIZE_MAX 255
+
 #define REPORT_PERIPHERALS_ID 1
 struct report_peripherals
 {
@@ -14,11 +16,17 @@ struct report_peripherals
   uint8_t joy_y;
   uint8_t buttons;
   uint8_t axis_z;
-} __attribute((packed))__;
+} __attribute__((packed));
+
+#define REPORT_PING_ID 2
+struct report_ping
+{
+  uint8_t id;
+  uint8_t dummy;
+} __attribute__((packed));
 
 void init_report_peripherals(struct report_peripherals* report);
-
-
+void init_report_ping(struct report_ping* report);
 
 #ifdef __cplusplus
 }
